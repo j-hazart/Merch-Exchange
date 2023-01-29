c914ddd2f0485402cc2b8eb9c6081f2b9c4ca172
j-hazart <jessy.hazart@gmail.com>
2023-01-29 02:16:34 +0100

    extracted data from db and display it on pages

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h1>Hello django</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello django</h1>
+        <p>Mes groupes préférés sont :</p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
 def listings(request):
     return HttpResponse('<h1>Listings</h1><p>En construction</p>')
 def contact_us(request):
